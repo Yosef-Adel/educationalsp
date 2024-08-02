a6d73f4526c2ffef911a3a9648522f9f9432c796
Youssef Saaid <youssefadel@apessmartsolutions.com>
2024-08-02 15:30:49 +0300

    adding a logger and test it

--- main.go
+++ main.go
@@ -3,19 +3,31 @@ package main
 import (
 	"bufio"
 	"educationalsp/rpc"
-	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("hi")
+	logger := getLogger("log.txt")
+	logger.Println("hey I started")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
 
 	for scanner.Scan() {
 		msg := scanner.Text()
-		handelMessage(msg)
+		handelMessage(logger, msg)
 	}
 }
 
-func handelMessage(_ any) {}
+func handelMessage(logger *log.Logger, msg any) {
+	logger.Println(msg)
+}
+
+func getLogger(filename string) *log.Logger {
+	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("hey, can't open the logfile")
+	}
+
+	return log.New(logfile, "[educationalsp]", log.Ldate|log.Ltime|log.Lshortfile)
+}
